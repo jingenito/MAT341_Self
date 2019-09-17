1fa3345685db5f76bb05e859d203339658df6a1d
Joe <jingenito1530@gmail.com>
2019-09-17 10:35:47 -0400

    dont remember what these changes were

--- Class/Poly_Codes/test_Poly.m
+++ Class/Poly_Codes/test_Poly.m
@@ -10,4 +10,24 @@ function test_Poly()
     
     %test poly code 3
     val3 = p3(a, x)
+    
+    %vector of x-values
+    xVec = -3:0.01:4.5;
+    
+    for i=1:length(xVec)
+        yVec(i) = p3(a, xVec(i));
+    end
+    
+    %
+    % Plotting attributes
+    %
+    lw = 4;
+    fs = 18;
+    
+    plot(xVec,yVec,'r', 'linewidth', lw)
+    %semilogx(xVec,yVec,'r', 'linewidth', lw)
+    xlabel('x');
+    ylabel('y');
+    leg = legend('2nd Order Poly');
+    set(gca,'fontsize',fs);
 end
