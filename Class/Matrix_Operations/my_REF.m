253afca6f91a60832a0d6370a9d801fd2b269225
Joe I <jingenito1530@gmail.com>
2019-11-30 13:52:03 -0500

    class work 11/26

---
+++ Class/Matrix_Operations/my_REF.m
@@ -0,0 +1,15 @@
+function A = my_REF(n)
+A = rand(n,n);
+
+for i=1:n
+    for j=1:n
+        %check A(j,j) = 0
+        if A(j,j) ~= 0 
+            %Rj = (1/ajj) * Rj
+            for k=j:n
+                A(j,k) = (1 / A(j,j))*A(j,k);
+            end
+            
+        end
+    end
+end
