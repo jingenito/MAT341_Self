f4dcee96ec0ec91936bfdd969e88f8cf5932451d
Joe I <jingenito1530@gmail.com>
2019-12-05 17:38:59 -0500

    update classwork 12/3

--- Class/Matrix_Operations/my_REF.m
+++ Class/Matrix_Operations/my_REF.m
@@ -1,5 +1,5 @@
 function A = my_REF(n)
-A = rand(n,n);
+A = rand(n,n)
 
 for i=1:n
     for j=1:n
@@ -8,6 +8,12 @@ for i=1:n
             %Rj = (1/ajj) * Rj
             for k=j:n
                 A(j,k) = (1 / A(j,j))*A(j,k);
+            end
+            
+            
+            %Rj1 = aj1jRj - Rj1
+            for k=j:n
+                
             end
             
         end
